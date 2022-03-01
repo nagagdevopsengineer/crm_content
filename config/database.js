bcd7d3f50f16f93d6b583fb512d3641cef396878
pawan <p@vapprtech.com>
2022-03-01 18:13:58 +0530

    databse

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'postgresql://arrivnow.cnscjbmeyzwl.us-east-1.rds.amazonaws.com'),
+      host: env('DATABASE_HOST', 'arrivnow.cnscjbmeyzwl.us-east-1.rds.amazonaws.com'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'crm-backend'),
       user: env('DATABASE_USERNAME', 'postgres'),
