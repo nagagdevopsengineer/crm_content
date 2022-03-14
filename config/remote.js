c3dba69303dbbcfdc7edd86a47c11634c60a5d62
rajeev-okoders <rajeev@okoders.com>
2022-03-14 13:12:41 +0530

    user mgmt port

--- config/remote.js
+++ config/remote.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
     remotehost: 'http://localhost', 
-    port: 8083,
+    port: 8081,
     userapi: '/api/users/register',
     consulhost: '0.0.0.0',
     consulport: 8500
