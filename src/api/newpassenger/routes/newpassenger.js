e1634b719946955cf3a81d5057c8e590fd5ab15d
rajeev-okoders <rajeev@okoders.com>
2022-04-05 13:55:29 +0530

    add passenger api

---
+++ src/api/newpassenger/routes/newpassenger.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * newpassenger router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::newpassenger.newpassenger');
---
+++ src/api/newpassenger/services/newpassenger.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * newpassenger service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::newpassenger.newpassenger');
