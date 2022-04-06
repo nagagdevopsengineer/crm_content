d287ba279395dbff6cf331b4c3b8c7a7e77d84cd
rajeev-okoders <rajeev@okoders.com>
2022-04-06 18:08:50 +0530

    client id

--- src/api/route/routes/custom-route.js
+++ src/api/route/routes/custom-route.js
@@ -2,7 +2,7 @@ module.exports = {
     routes: [
       { // Path defined with a URL parameter
         method: 'GET',
-        path: '/routes/nmroutebus/:contractorid',
+        path: '/routes/nmroutebus/:clientid',
         handler: 'route.findAvailableRoutes',
       }
     ]
