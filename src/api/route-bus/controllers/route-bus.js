35fb0b426c35230f07e9e49552c52c88d205c71d
rajeev-okoders <rajeev@okoders.com>
2022-04-15 18:16:35 +0530

    stops addedd to route bus response

--- src/api/route-bus/controllers/route-bus.js
+++ src/api/route-bus/controllers/route-bus.js
@@ -6,4 +6,42 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::route-bus.route-bus');
+//module.exports = createCoreController('api::route-bus.route-bus');
+module.exports = createCoreController('api::route-bus.route-bus', ({ env }) =>  ({
+
+    async find(ctx) {
+        // some custom logic here
+        //ctx.query = { ...ctx.query, local: 'en' }
+        
+        // Calling the default core action
+        const { data, meta } = await super.find(ctx);
+        var routIds = [];
+        var data2 = data;
+
+        for(let i=0; i< data2.length; i++) {
+       console.log(data2[i].attributes.route.data.id);
+            
+        const stops = await strapi.entityService.findMany('api::stop.stop',{
+            filters :{ 
+                route :{ 
+                  id :  data2[i].attributes.route.data.id 
+                },
+            },
+            orderBy :{order:'asc'}
+        });
+  
+        
+        //element.attributes.route.data.stops = stops;
+        data[i].attributes.route.data.stops = stops;
+
+  
+          };
+
+      
+        return { data, meta };
+      },
+  
+
+
+}));
+
