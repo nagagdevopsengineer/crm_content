cf598f054d9c686e596a2a642f8537eec266b7f0
rajeev-okoders <rajeev@okoders.com>
2022-05-18 10:24:38 +0530

    employee bus boarding notification

--- src/api/employeeotp/controllers/employeeotp.js
+++ src/api/employeeotp/controllers/employeeotp.js
@@ -6,4 +6,20 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::employeeotp.employeeotp');
+module.exports = createCoreController('api::employeeotp.employeeotp', ({ env }) =>  ({
+
+
+    async update(ctx){
+    
+    const response =  await super.update(ctx);   
+    
+    if(response.isBoarded){
+
+        //get employee details and invoke notification API here.
+
+    }
+
+    return response;
+    },
+
+}))
