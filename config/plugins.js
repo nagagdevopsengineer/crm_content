b3410594a75e06c35a7f15ab81dfa0688e2b83bf
rajeev-okoders <rajeev@okoders.com>
2022-05-31 18:35:07 +0530

    s3 integrations

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-    upload: {
+   /** upload: {
       config: {
         breakpoints: {
           xlarge: 1920,
@@ -9,5 +9,31 @@ module.exports = ({ env }) => ({
           xsmall: 64
         },
       },
+    },*/ 
+
+    upload: {
+      config: {
+        breakpoints: {
+          xlarge: 1920,
+          large: 1000,
+          medium: 750,
+          small: 500,
+          xsmall: 64
+        },
+        provider: 'aws-s3',
+        providerOptions: {
+          accessKeyId: 'AKIAS3SGCEIKIAV5VH2E',
+          secretAccessKey: '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',
+          region: 'us-east-2',
+          params: {
+            Bucket: 'arrivnowcontents',
+          },
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      }
     },
   });
