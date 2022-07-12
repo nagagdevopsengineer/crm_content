e8d2c7f3fbeb2bf4d61d3c3df6eef6a2dd4af20a
pavankumar <pavan.kumar@vapprtech.com>
2022-07-12 18:44:50 +0530

    database updated

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'sales-demo1.c7fgca1gf3e6.us-east-2.rds.amazonaws.com'),
+      host: env('DATABASE_HOST', 'sales-demo.c7fgca1gf3e6.us-east-2.rds.amazonaws.com'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'sales-demo1'),
+      database: env('DATABASE_NAME', 'sales-demo'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'Welcome2vapprtech'),
+      password: env('DATABASE_PASSWORD', 'welcome2vapprtech'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
