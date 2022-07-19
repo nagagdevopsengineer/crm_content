0d66d8be0946f0ef5a20c2c7ddd23ad5dabf4d98
shivakanya <shivakanya@vapprtech.com>
2022-07-19 12:23:58 +0530

    route & rideDetails

---
+++ src/api/ride-detail/routes/ride-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ride-detail router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ride-detail.ride-detail');
