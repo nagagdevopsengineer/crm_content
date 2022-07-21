9a0a70eba577fe5e8d84cddb2b14ce604dc1e692
spandey <shalabh.pandey@ksolves.com>
2022-07-21 09:00:41 +0530

    generate synthetic data for: 1. client 2. contractor 3. buses 5. driver 6. helper 7. employee 8. employee-setting 9. shift time 10. client shift timing 11. employee emergency contact 12. employee ride detail 13. routes 14. sos logs 15. route-bus 16. trips

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -4,13 +4,15 @@ const faker = require('faker');
 
 const bootstrap =  async () => {
   console.log("Current environment", process.env.NODE_ENV);
-  // if (process.env.NODE_ENV == 'sales-demo') {
   
     const status = ['pending', 'approved', 'rejected']
     const vaccinationstatus = ['No', 'Partial', 'Vaccinated']
     const driverHelperStatus = ['Created', 'Approved', 'Rejected']
     const busStatus = ['pending', 'approved', 'reject']
     const flag = [true, false]
+    const shiftName = ['morning', 'afternoon', 'evening', 'night']
+    const rideDetailType = ['ola', 'uber']
+    const routeNames = ['Sanjay Gram To RPS', 'EmaarIndia To OM Sweet', 'Magnum Tower To Samsung Office', 'Sector Fifty Three Rapid Metro to Magnum Towers', 'Huda Metro to Guru Dronacharya Metro']
 
     //Deleting any existing data in the database for clients
     let entries = await strapi.entityService.findMany('api::client.client', {
@@ -101,6 +103,105 @@ const bootstrap =  async () => {
       })
     }
 
+    //Deleting any existing data in the database for shift time
+    entries = await strapi.entityService.findMany('api::shifttime.shifttime', {
+      fields: ['id']    
+    });
+    if (entries && entries.length) {
+      console.log("Deleting any existing data in the database for shift time")
+      entries.forEach( async (entry) => {
+        await strapi.entityService.delete('api::shifttime.shifttime', entry.id);
+      })
+    }
+
+    //Deleting any existing data in the database for client shift timing
+    entries = await strapi.entityService.findMany('api::client-shift-timing.client-shift-timing', {
+      fields: ['id']    
+    });
+    if (entries && entries.length) {
+      console.log("Deleting any existing data in the database for client shift timing")
+      entries.forEach( async (entry) => {
+        await strapi.entityService.delete('api::client-shift-timing.client-shift-timing', entry.id);
+      })
+    }
+
+    //Deleting any existing data in the database for employee emergency contact
+    entries = await strapi.entityService.findMany('api::employee-emergency-contact.employee-emergency-contact', {
+      fields: ['id']    
+    });
+    if (entries && entries.length) {
+      console.log("Deleting any existing data in the database for employee emergency contact")
+      entries.forEach( async (entry) => {
+        await strapi.entityService.delete('api::employee-emergency-contact.employee-emergency-contact', entry.id);
+      })
+    }
+
+    //Deleting any existing data in the database for employee ride detail
+    entries = await strapi.entityService.findMany('api::ride-detail.ride-detail', {
+      fields: ['id']    
+    });
+    if (entries && entries.length) {
+      console.log("Deleting any existing data in the database for employee ride detail")
+      entries.forEach( async (entry) => {
+        await strapi.entityService.delete('api::ride-detail.ride-detail', entry.id);
+      })
+    }
+
+    //Deleting any existing data in the database for routes
+    entries = await strapi.entityService.findMany('api::route.route', {
+      fields: ['id']    
+    });
+    if (entries && entries.length) {
+      console.log("Deleting any existing data in the database for routes")
+      entries.forEach( async (entry) => {
+        await strapi.entityService.delete('api::route.route', entry.id);
+      })
+    }
+
+    //Deleting any existing data in the database for stops
+    entries = await strapi.entityService.findMany('api::stop.stop', {
+      fields: ['id']    
+    });
+    if (entries && entries.length) {
+      console.log("Deleting any existing data in the database for stops")
+      entries.forEach( async (entry) => {
+        await strapi.entityService.delete('api::stop.stop', entry.id);
+      })
+    }
+
+    //Deleting any existing data in the database for sos logs
+    entries = await strapi.entityService.findMany('api::sos-log.sos-log', {
+      fields: ['id']    
+    });
+    if (entries && entries.length) {
+      console.log("Deleting any existing data in the database for sos logs")
+      entries.forEach( async (entry) => {
+        await strapi.entityService.delete('api::sos-log.sos-log', entry.id);
+      })
+    }
+
+    //Deleting any existing data in the database for route-bus
+    entries = await strapi.entityService.findMany('api::route-bus.route-bus', {
+      fields: ['id']    
+    });
+    if (entries && entries.length) {
+      console.log("Deleting any existing data in the database for route-bus")
+      entries.forEach( async (entry) => {
+        await strapi.entityService.delete('api::route-bus.route-bus', entry.id);
+      })
+    }
+
+    //Deleting any existing data in the database for trips
+    entries = await strapi.entityService.findMany('api::trip.trip', {
+      fields: ['id']    
+    });
+    if (entries && entries.length) {
+      console.log("Deleting any existing data in the database for trips")
+      entries.forEach( async (entry) => {
+        await strapi.entityService.delete('api::trip.trip', entry.id);
+      })
+    }
+
     
     //Creating synthetic data for client
     console.log("Creating synthetic data for client")
@@ -122,8 +223,9 @@ const bootstrap =  async () => {
             mobile: Math.floor(Math.random() * 9000000000) + 1000000000,
             address: `${faker.address.streetName()} ${faker.address.streetAddress()}`,
             companyname: faker.company.companyName(),
-            pancard: faker.random.alphaNumeric(10)
-          },
+            pancard: faker.random.alphaNumeric(10),
+            publishedAt: new Date().toISOString()
+          }
         })}
       catch(err) {
         console.log("Error while synthetic data for clients", err)
@@ -152,7 +254,8 @@ const bootstrap =  async () => {
             address: `${faker.address.streetName()} ${faker.address.streetAddress()}`,
             companyname: faker.company.companyName(),
             pancard: faker.random.alphaNumeric(10),
-            client: clients[Math.floor(Math.random()*clients.length)].id
+            client: clients[Math.floor(Math.random()*clients.length)].id,
+            publishedAt: new Date().toISOString()
           },
         })}
       catch(err) {
@@ -176,7 +279,8 @@ const bootstrap =  async () => {
           status: busStatus[Math.floor(Math.random()*busStatus.length)],
           contractor: contractors[Math.floor(Math.random()*contractors.length)].id,
           uuid: uuidv4(),
-          puc: 'random'
+          puc: 'random',
+          publishedAt: new Date().toISOString()
         },
       })}
       catch(err) {
@@ -202,7 +306,8 @@ const bootstrap =  async () => {
           statusreason: faker.random.word(),
           status: driverHelperStatus[Math.floor(Math.random()*driverHelperStatus.length)],
           mobile: Math.floor(Math.random() * 9000000000) + 1000000000,
-          driverid: `DRIVER: ${faker.random.alphaNumeric(5).toUpperCase()}`
+          driverid: `DRIVER: ${faker.random.alphaNumeric(5).toUpperCase()}`,
+          publishedAt: new Date().toISOString()
         },
       })}
       catch(err) {
@@ -228,7 +333,8 @@ const bootstrap =  async () => {
           statusreason: faker.random.word(),
           status: driverHelperStatus[Math.floor(Math.random()*driverHelperStatus.length)],
           contact_number: Math.floor(Math.random() * 9000000000) + 1000000000,
-          helperid: `HELPER: ${faker.random.alphaNumeric(5).toUpperCase()}`
+          helperid: `HELPER: ${faker.random.alphaNumeric(5).toUpperCase()}`,
+          publishedAt: new Date().toISOString()
         },
       })}
       catch(err) {
@@ -257,7 +363,8 @@ const bootstrap =  async () => {
           address: `${faker.address.streetName()} ${faker.address.streetAddress()}`,
           lastmile: flag[Math.floor(Math.random()*flag.length)],
           client: clients[Math.floor(Math.random()*clients.length)].id,
-          bus: buses[Math.floor(Math.random()*buses.length)].id
+          bus: buses[Math.floor(Math.random()*buses.length)].id,
+          publishedAt: new Date().toISOString()
         },
       })}
       catch(err) {
@@ -275,7 +382,11 @@ const bootstrap =  async () => {
           id: index,
           bus: index,
           driver: index,
-          helper: index
+          helper: index,
+          from_date: faker.date.past().toISOString().split('T')[0],
+          to_date: faker.date.future().toISOString().split('T')[0],
+          timing: `${"5678".toHHMMSS()}`,
+          publishedAt: new Date().toISOString()
         },
       })}
       catch(err) {
@@ -297,13 +408,230 @@ const bootstrap =  async () => {
           driver_journey_started: flag[Math.floor(Math.random()*flag.length)],
           previous_stop_alert: flag[Math.floor(Math.random()*flag.length)],
           employee: employees[Math.floor(Math.random()*employees.length)].id,
+          publishedAt: new Date().toISOString()
         },
       })}
       catch(err) {
         console.log("Error while synthetic data for bus-driver", err)
       }
     }
-  // }
+
+    // Creating synthetic data for shift time
+    console.log("Creating synthetic data for shift time")
+    for (let index = start_index ; index < end_index; index++) {
+      try {
+        await strapi.entityService.create('api::shifttime.shifttime', {
+        data: {
+          id: index,
+          shiftname: shiftName[Math.floor(Math.random()*shiftName.length)],
+          startTime: faker.date.past().toISOString().split('T')[0],
+          endTime: faker.date.future().toISOString().split('T')[0],
+          isActive: flag[Math.floor(Math.random()*flag.length)],
+          publishedAt: new Date().toISOString()
+        },
+      })}
+      catch(err) {
+        console.log("Error while synthetic data for shift time", err)
+      }
+    }
+
+    // Creating synthetic data for client shift timing
+    console.log("Creating synthetic data for client shift timing")
+    for (let index = start_index ; index < end_index; index++) {
+      try {
+        await strapi.entityService.create('api::client-shift-timing.client-shift-timing', {
+        data: {
+          id: index,
+          client: Math.floor(Math.random() * (50 - 1 + 1)) + 1,
+          shifttime: Math.floor(Math.random() * (50 - 1 + 1)) + 1,
+          publishedAt: new Date().toISOString()
+        },
+      })}
+      catch(err) {
+        console.log("Error while synthetic data for client shift timing", err)
+      }
+    }
+
+    // Creating synthetic data for employee emergency contact
+    console.log("Creating synthetic data for employee emergency contact")
+    for (let index = start_index ; index < 151; index++) {
+      try {
+        await strapi.entityService.create('api::employee-emergency-contact.employee-emergency-contact', {
+        data: {
+          id: index,
+          employee: index % 50 + 1,
+          mobile: Math.floor(Math.random() * 9000000000) + 1000000000,
+          publishedAt: new Date().toISOString()
+        },
+      })}
+      catch(err) {
+        console.log("Error while synthetic data for employee emergency contact", err)
+      }
+    }
+
+    // Creating synthetic data for employee ride detail
+    console.log("Creating synthetic data for employee ride detail")
+    for (let index = start_index ; index < 151; index++) {
+      try {
+        await strapi.entityService.create('api::ride-detail.ride-detail', {
+        data: {
+          id: index,
+          employee: index % 50 + 1,
+          ride: rideDetailType[Math.floor(Math.random()*rideDetailType.length)],
+          date: faker.date.past().toISOString().split('T')[0],
+          publishedAt: new Date().toISOString()
+        },
+      })}
+      catch(err) {
+        console.log("Error while synthetic data for employee ride detail", err)
+      }
+    }
+
+    // Creating synthetic data for routes
+    console.log("Creating synthetic data for routes")
+    for (let index = start_index ; index < 6; index++) {
+      try {
+        await strapi.entityService.create('api::route.route', {
+        data: {
+          id: index,
+          name: routeNames[index - 1],
+          description: routeNames[index - 1],
+          client: index,
+          Km: Math.floor(Math.random() * (50 - 1 + 1)) + 1,
+          publishedAt: new Date().toISOString()
+        },
+      })}
+      catch(err) {
+        console.log("Error while synthetic data for routes", err)
+      }
+    }
+
+    // Creating synthetic data for stops
+    const stops = [ {name: `Sanjay Gram` , longitude: 77.0417, latitude: 28.4781, route: 1, order: 1},
+                    {name: `C Block Road, Sanjay Gram Colony, Rajiv Nagar, Sector 13, Gurugram, Haryana` , longitude: 77.0425654, latitude: 28.4778707, route: 1, order: 2},
+                    {name: `Khelgram Sports Academy, Sanjay Gram Colony, Rajiv Nagar, Sector 13, Gurugram, Haryana, India`, longitude: 77.0425333, latitude: 28.4788431, route: 1, order: 3},
+                    {name: `Sanjay Gram Colony, Rajiv Nagar, Sector 13, Gurugram, Haryana 122022, India`, longitude: 77.0416967, latitude: 28.4780596, route: 1,order: 4},
+                    {name: `RPS` , longitude: 77.0529341698, latitude: 28.4193531438, route: 1, order: 5},
+                    {name: `EmaarIndia`,longitude: 77.1290223,latitude: 28.7248935,route: 2,order: 1},
+                    {name: `Ambience Mall`,longitude: 77.0958,latitude: 28.5058,route: 2,order: 2},
+                    {name: `National Rail Museum`,longitude:  77.1814,latitude: 28.5850,route: 2,order: 3},
+                    {name: `Nizamuddin Railway Station`,longitude: 77.2534,latitude: 28.5889,route: 2,order: 4},
+                    {name: `Om Sweets & Restaurant`,longitude: 77.50411,latitude: 28.60139,route: 2,order: 5},
+                    {name: `Magnum Towers`,longitude: 77.1084,latitude: 28.4120,route: 3,order: 1},
+                    {name: `DoubleTree by Hilton Hotel`,longitude: 77.1045,latitude: 28.4220,route: 3,order: 2},
+                    {name: `Centrum Plaza`,longitude: 77.1042,latitude: 28.4339,route: 3,order: 3},
+                    {name: `Hotel Metro Residency`,longitude: 77.09472,latitude: 28.45592,route: 3,order: 4},
+                    {name: `Samsung India Electronics Gurugram`,longitude: 77.09609,latitude: 28.45230,route: 3,order: 5},
+                    {name: `Sector Fifty Three Rapid Metro`,longitude: 77.50411,latitude: 28.60139,route: 4,order: 1},
+                    {name: `Centrum Plaza`,longitude: 77.1042,latitude: 28.4339,route: 4,order: 2},
+                    {name: `Hotel Metro Residency`,longitude: 77.09472,latitude: 28.45592,route: 4,order: 3},
+                    {name: `Samsung India Electronics Gurugram`,longitude: 77.09609,latitude: 28.45230,route: 4,order: 5},
+                    {name: `Magnum Towers`,longitude: 77.1084,latitude: 28.4120,route: 4,order: 5},
+                    {name: `Huda Metro`,longitude: 77.0724791115211, latitude: 28.459378884371876, route: 5,order: 1},
+                    {name: `Supermart-1, DLF Phase IV`,longitude: 77.08754250049124, latitude: 28.462299539344333, route: 5,order: 2},
+                    {name: `DLF Phase 5, Sector 43`,longitude: 77.09492393983676, latitude: 28.463469107200353, route: 5,order: 3},
+                    {name: `Phase 1, Chakkarpur, Sector 28`,longitude: 77.09393688690102, latitude: 28.47135392043445, route: 5,order: 4},
+                    {name: `Guru Dronacharya Metro Station Gate No 2`,longitude:77.10264870194253, latitude:  28.482369037802915, route: 5,order: 5}]
+    console.log("Creating synthetic data for stops")
+    for (let index = 0 ; index < stops.length + 1; index++) {
+      try {
+        await strapi.entityService.create('api::stop.stop', {
+          data: {
+            id: index + 1,
+            name: stops[index].name,
+            longitude: stops[index].longitude,
+            latitude: stops[index].latitude,
+            route: stops[index].route,
+            order: stops[index].order,
+            publishedAt: new Date().toISOString()
+          }
+        })}
+      catch(err) {
+        console.log("Error while synthetic data for stops", err)
+      }
+    }
+
+    // Creating synthetic data for sos logs
+    console.log("Creating synthetic data for sos logs")
+    for (let index = 1 ; index < 151; index++) {
+      const nearbyGPSCoordinate = faker.address.nearbyGPSCoordinate()
+      try {
+        await strapi.entityService.create('api::sos-log.sos-log', {
+          data: {
+            id: index,
+            sms: faker.lorem.words(),
+            location: faker.address.cityName(),
+            lat: nearbyGPSCoordinate[0],
+            long: nearbyGPSCoordinate[1],
+            employee: index % 50 + 1,
+            mobile: Math.floor(Math.random() * 9000000000) + 1000000000,
+            publishedAt: new Date().toISOString()
+          }
+        })}
+      catch(err) {
+        console.log("Error while synthetic data for sos logs", err)
+      }
+    }
+    
+    // Creating synthetic data for route-bus
+    console.log("Creating synthetic data for route-bus")
+    for (let index = start_index ; index < end_index; index++) {
+      try {
+        await strapi.entityService.create('api::route-bus.route-bus', {
+          data: {
+            id: index,
+            bus: index,
+            route: index % 5 + 1,
+            from_date: faker.date.past().toISOString().split('T')[0],
+            to_date: faker.date.future().toISOString().split('T')[0],
+            time: `${"5678".toHHMMSS()}`,
+            publishedAt: new Date().toISOString()
+          }
+        })}
+      catch(err) {
+        console.log("Error while synthetic data for route-bus", err)
+      }
+    }
+
+    // Creating synthetic data for trips
+    console.log("Creating synthetic data for trips")
+    for (let index = start_index ; index < 201; index++) {
+      let date = faker.date.past().toISOString();
+      try {
+        await strapi.entityService.create('api::trip.trip', {
+          data: {
+            id: index,
+            tripdate: date.split('T')[0],
+            uuid: uuidv4(),
+            starttime: date,
+            endtime: faker.date.recent().toISOString(),
+            route_bus: Math.floor(Math.random() * (50 - 1 + 1)) + 1,
+            isstarted: flag[Math.floor(Math.random()*flag.length)],
+            isended: flag[Math.floor(Math.random()*flag.length)],
+            bus_driver: Math.floor(Math.random() * (50 - 1 + 1)) + 1,
+            scheduledtime: faker.date.past().toISOString(),
+            Km: Math.floor(Math.random() * (50 - 1 + 1)) + 1,
+            publishedAt: new Date().toISOString()
+          }
+        })}
+      catch(err) {
+        console.log("Error while synthetic data for trips", err)
+      }
+    }
+
 }
 
 module.exports =  bootstrap
+
+
+String.prototype.toHHMMSS = function () {
+  let sec_num = parseInt(this, 10);
+  let hours   = Math.floor(sec_num / 3600);
+  let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  let seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+  if (hours   < 10) {hours   = "0"+hours;}
+  if (minutes < 10) {minutes = "0"+minutes;}
+  if (seconds < 10) {seconds = "0"+seconds;}
+  return hours + ':' + minutes + ':' + seconds;
+}
