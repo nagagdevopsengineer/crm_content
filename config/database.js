985b6b9cf53791c4164a48d46aa0196b3c57fb94
rajeev-okoders <rajeev@okoders.com>
2023-04-21 15:32:02 +0530

    credentials changes

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'arrivnow.cnscjbmeyzwl.us-east-1.rds.amazonaws.com'),
+      host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'crm-backend'),
+      database: env('DATABASE_NAME', 'crmcontent'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'welcome2arrivnow'),
+      password: env('DATABASE_PASSWORD', 'P@ssw0rd@123'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
--- config/remote.js
+++ config/remote.js
@@ -1,11 +1,11 @@
 module.exports = ({ env }) => ({
-    remotehost: 'http://user.arrivnow.com', 
-    port: '',
-    userapi: '/api/users/register',
-    consulhost: '0.0.0.0',
-    consulport: 8500,
-    freshdesktickets: 'https://vapprtech.freshdesk.com/api/v2/tickets',
-    freshdeskusername: 'a57SSfoUyZ5WdiQJE75',
-    freshdeskpassword: 'a57SSfoUyZ5WdiQJE75'
+  remotehost: 'https://user.arrivnow.com',
+  port: '',
+  userapi: '/api/users/register',
+  consulhost: '0.0.0.0',
+  consulport: 8500,
+  freshdesktickets: 'https://vapprtech.freshdesk.com/api/v2/tickets',
+  freshdeskusername: 'a57SSfoUyZ5WdiQJE75',
+  freshdeskpassword: 'a57SSfoUyZ5WdiQJE75'
 
-  });
+});
